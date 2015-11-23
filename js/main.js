68443d456a050b0b5bbd5d70170ef543fc1e16aa
Lena Seternes <lena.seternes@geomatikk-ikt.no>
2015-11-23 13:29:33 +0100

    Changed map setup

--- js/main.js
+++ js/main.js
@@ -1,15 +1,44 @@
-var map;
+var setupMap = function() {
 
-function init() {
- map = new ol.Map({
-    target: 'section_basemap',
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.MapQuest({layer: 'sat'})
+    var EPSG = 'EPSG:32633';
+    var EXTENT = [-2500000.0,3500000.0,3045984.0,9045984.0];
+    var RESOLUTIONS = [21664,10832,5416,2708,1354,677,338.5,169.25,84.625,42.3125,21.15625,10.578125,5.2890625,2.64453125,1.322265625,0.6611328125,0.33056640625,0.165283203125];
+    var CENTER = [270954.00, 7038739.20];
+
+    var layers = makeLayers();
+
+    var projection = new ol.proj.Projection({
+        code: EPSG,
+        extent: EXTENT,
+        units: 'm'
+    });
+
+    var map = new ol.Map({
+        target: 'map',
+        layers: layers,
+        view: new ol.View({
+            projection: projection,
+            resolutions: RESOLUTIONS,
+            center: CENTER,
+            zoom: 3
         })
-    ],
-    view: new ol.View({
-        center: ol.proj.fromLonLat([37.41, 8.82]),
-        zoom: 4
-    })
-})};
+    });
+};
+
+var makeLayers = function() {
+    var layers = [];
+    var wms = new ol.layer.Tile({
+        title: "Norges grunnkart",
+        source: new ol.source.TileWMS({
+            url: "http://wms.geonorge.no/skwms1/wms.topo2?",
+            params: {
+                LAYERS: "topo2_WMS",
+                FORMAT: 'image/png',
+                STYLE: 'default'
+            }
+        })
+    });
+    layers.push(wms);
+
+    return layers;
+};
